ba718d7afa32fc4c40e293f99e6cb46f5acb9768
Alina <alina.baicu93@yahoo.com>
2018-03-19 11:12:59 +0000

    commented out seedDB

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 app.use(methodOverride("_method"));
-app.use(flash());
+app.use(flash());  
 //seedDB();
 
 // passport configuration
